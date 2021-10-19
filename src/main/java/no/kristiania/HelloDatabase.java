0922d0de86fc0068d09a9f1107287e39ebdc4fe5
viskog <89581174+viskog@users.noreply.github.com>
2021-10-19 16:48:48 +0200

    Rretrive data from database with java

---
+++ src/main/java/no/kristiania/HelloDatabase.java
@@ -0,0 +1,30 @@
+package no.kristiania;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class HelloDatabase {
+
+    public static void main(String[] args) throws SQLException {
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/person_db");
+        dataSource.setUser("person_dbuser");
+        dataSource.setPassword("SrAwsd?s5#");
+
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from people")) {
+
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+                        System.out.println(rs.getString(2));
+                    }
+                }
+            }
+        }
+    }
+}
